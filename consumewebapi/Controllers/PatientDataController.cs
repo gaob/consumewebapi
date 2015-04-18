2d6ef261a45b153adc496151c4a3fdd7950b32ec
gaob <nightfir@mail.com>
2015-04-19 01:13:03 +0800

    PatientData initial check-in.

---
+++ consumewebapi/Controllers/PatientDataController.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ConsumeWebAPI.Helper;
+using ConsumeWebAPI.Models;
+
+namespace ConsumeWebAPI.Controllers
+{
+    public class PatientDataController : Controller
+    {
+        //
+        // GET: /PatientData/
+
+        public ActionResult Index()
+        {
+            //Dummy data.
+            List<PatientDataModel> aList = new List<PatientDataModel>();
+
+            aList.Add(new PatientDataModel());
+
+            return View(aList);
+        }
+
+        //
+        // GET: /PatientData/Details/5
+
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        //
+        // GET: /PatientData/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /PatientData/Create
+
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /PatientData/Edit/5
+
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /PatientData/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /PatientData/Delete/5
+
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /PatientData/Delete/5
+
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ consumewebapi/Models/PatientDataModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace ConsumeWebAPI.Models
+{
+    public class PatientDataModel
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Doctor ID")]
+        public string DoctorID { get; set; }
+
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+
+        [Display(Name = "Start Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime StartDate { get; set; }
+
+        [Display(Name = "End date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime EndDate { get; set; }
+
+        [Display(Name = "Type")]
+        public string Type { get; set; }
+
+        [Display(Name = "Time")]
+        [DataType(DataType.Time)]
+        [DisplayFormat(DataFormatString = "{0:hh:mm}", ApplyFormatInEditMode = true)]
+        public DateTime Time { get; set; }
+
+        [Display(Name = "Record ID")]
+        public string RecordID { get; set; }
+
+        [Display(Name = "Record Name")]
+        public string RecordName { get; set; }
+    }
+}
